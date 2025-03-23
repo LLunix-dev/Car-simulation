99c163a3406e65c46ea1998313baa1e59053e8f3
LLunix-dev <llunix.mc.123@gmail.com>
2025-03-23 19:16:12 +0100

    Nothing you need to know...

--- linux/src/main.cpp
+++ linux/src/main.cpp
@@ -9,48 +9,47 @@ int angle_left = 0;
 int angle_right = 0;
 float angle_range = 3;
 
-void cleanup(SDL_Texture *imageTexture, SDL_Renderer *renderer, SDL_Window *window)
+void cleanup(SDL_Texture *carTexture, SDL_Renderer *renderer, SDL_Window *window)
 {
-    SDL_DestroyTexture(imageTexture);
+    SDL_DestroyTexture(carTexture);
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     exit(0);
 }
 
-void handleInput(SDL_Event event, SDL_Texture *imageTexture, SDL_Renderer *renderer, SDL_Window *window)
+void handleInput(SDL_Event event, SDL_Texture *carTexture, SDL_Renderer *renderer, SDL_Window *window)
 {
     while (SDL_PollEvent(&event))
-{
-    if (event.type == SDL_QUIT || 
-        (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
     {
-        cleanup(imageTexture, renderer, window);
-    }
-    else if (event.type == SDL_KEYDOWN)
-    {
-        switch (event.key.keysym.sym)
+        if (event.type == SDL_QUIT || 
+            (event.type == SDL_KEYDOWN && event.key.keysym.sym == SDLK_ESCAPE))
         {
-            case SDLK_q: angle_left = angle_range; break;
-            case SDLK_e: angle_right = angle_range; break;
-            case SDLK_w: changeSpeed = 0.1; break;
-            case SDLK_s: changeSpeed = -0.1; break;
-            case SDLK_SPACE: changeSpeed = -1; break;
+            cleanup(carTexture, renderer, window);
         }
-    }
-    else if (event.type == SDL_KEYUP)
-    {
-        switch (event.key.keysym.sym)
+        else if (event.type == SDL_KEYDOWN)
         {
-            case SDLK_q: angle_left = 0; break;
-            case SDLK_e: angle_right = 0; break;
-            case SDLK_w:
-            case SDLK_s: changeSpeed = 0; break;
+            switch (event.key.keysym.sym)
+            {
+                case SDLK_q: angle_left = angle_range; break;
+                case SDLK_e: angle_right = angle_range; break;
+                case SDLK_w: changeSpeed = 0.1; break;
+                case SDLK_s: changeSpeed = -0.1; break;
+                case SDLK_SPACE: changeSpeed = -1; break;
+            }
+        }
+        else if (event.type == SDL_KEYUP)
+        {
+            switch (event.key.keysym.sym)
+            {
+                case SDLK_q: angle_left = 0; break;
+                case SDLK_e: angle_right = 0; break;
+                case SDLK_w:
+                case SDLK_s: changeSpeed = 0; break;
+            }
         }
     }
 }
-}
-
 
 void drawRotatedImage(SDL_Renderer *renderer, SDL_Texture *texture, int x, int y, int w, int h, double angle)
 {
@@ -70,56 +69,61 @@ int main()
     SDL_Window *window = SDL_CreateWindow("Car simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1000, 800, SDL_WINDOW_RESIZABLE);
     SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 
-    SDL_Surface *imageSurface = SDL_LoadBMP("./res/car.bmp");
-    if (!imageSurface)
-    {
-        std::cout << "Error: " << SDL_GetError() << std::endl;
-        SDL_DestroyRenderer(renderer);
-        SDL_DestroyWindow(window);
-        SDL_Quit();
-        return 1;
-    }
+    SDL_Surface *carSurface = SDL_LoadBMP("res/car.bmp");
+    SDL_Surface *trackSurface = SDL_LoadBMP("res/race-track.bmp");
 
-    SDL_Texture *imageTexture = SDL_CreateTextureFromSurface(renderer, imageSurface);
-    SDL_FreeSurface(imageSurface);
+    SDL_Texture *carTexture = SDL_CreateTextureFromSurface(renderer, carSurface);
+    SDL_FreeSurface(carSurface);
 
-    float x = 800.0 /2;
-    float y =  600.0 /2;
+    SDL_Texture *trackTexture = SDL_CreateTextureFromSurface(renderer, trackSurface);
+    SDL_FreeSurface(trackSurface);
+
+    float x = 800.0 / 2;
+    float y = 600.0 / 2;
 
     SDL_Event event;
-
+    float vx = 0.0f, vy = 0.0f; 
+    float friction = 0.985f;
+    
     while (true)
     {
-        // Event-Handling
         SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
         SDL_RenderClear(renderer);
-        
-        handleInput(event, imageTexture, renderer, window);
+    
+        handleInput(event, carTexture, renderer, window);
+    
         angle += angle_right;
         angle -= angle_left;
-
+    
         float radians = (M_PI * angle) / 180.0;
-
-        float vx = speed * cos(radians);
-        float vy = speed * sin(radians);
-
-
+    
+        if (changeSpeed != 0)
+        {
+            vx += changeSpeed * cos(radians);
+            vy += changeSpeed * sin(radians);
+        }
+    
+        vx *= friction;
+        vy *= friction;
+    
         x += vx;
         y += vy;
 
-        speed += changeSpeed;
-        if(speed < 0.0) speed = 0;
-
-        if(!(speed == 0.0)){
+        if (speed > 0.0f)
+        {
             angle_range = 2 + (speed / 8.0);
-        }else{
+        }
+        else
+        {
             angle_range = 0;
         }
-
-        drawRotatedImage(renderer, imageTexture, x, y, 200, 100, angle);
-
+    
+        drawRotatedImage(renderer, trackTexture, 0, 0, 1000, 800, 0);
+        drawRotatedImage(renderer, carTexture, x, y, 200 / 1.5, 100 / 1.5, angle);
+    
         SDL_RenderPresent(renderer);
         SDL_Delay(16);
     }
+    
     return 0;
 }
